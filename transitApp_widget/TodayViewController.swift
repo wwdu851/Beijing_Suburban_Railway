af5e2c4892de1a593d16fea356ac307c8e8c116a
stringconstant <wdu@students.stab.org>
2018-10-09 23:24:24 -0400

    Latest Commit

---
+++ transitApp_widget/TodayViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TodayViewController.swift
+//  transitApp_widget
+//
+//  Created by William Du on 2018/6/7.
+//  Copyright © 2018年 William Du. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
